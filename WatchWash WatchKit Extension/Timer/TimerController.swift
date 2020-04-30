479f3161321ab6b1b017c6e70c5d609d882b7b15
Frank Foster <frank.e.foster@icloud.com>
2020-04-30 14:03:22 -0700

    updated Timer

--- WatchWash WatchKit Extension/Timer/TimerController.swift
+++ WatchWash WatchKit Extension/Timer/TimerController.swift
@@ -17,79 +17,250 @@ class TimerController: WKInterfaceController {
     @IBOutlet weak var textLabel: WKInterfaceLabel!
     @IBOutlet weak var timerGroup: WKInterfaceGroup!
     
-    
-    let currentDate = Date()
-    let soapArray = ["Get some soap!","Prep phase","Lather up!", "Soapy, soap","Wash in 3!"]
-    let sayingsArray = ["Wash those hands!", "Clean up!","","Water time!","Rinse, Repeat!"]
-    
+    var intervalTimer = Timer()
     var isRunning = true
-    var timer = Timer()
-    var labelText:TimeInterval?
+    var isWorkingOut = false
+
+    var startWash = "Start"
+    var stopWash = "Stop"
     
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
+    var intervalValue:Double = 20.0
+    
+    
+    func startTimer(value: Bool) {
         
+        isRunning = value
+               if value{
+                   textLabel.setText(icon())
+               }else{
+                   textLabel.setText(icon())
+
+               }
+               //If in a workout, reset the timer
+               if isWorkingOut{
+                   timerReset()
+               }
         
-        labelText = context as? TimeInterval
-        let addedTime = context as? Double
-        
-        firstTimer()
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-        
-            self.secondTimer()
+    }
+    
+    
+    func startRunning () {
+        isWorkingOut = !isWorkingOut
+        if isWorkingOut{
+            //set up for workout
+            textLabel.setText(icon())
+
+            
+            //start the timer
+            timerReset()
+        } else {
+            //show pizza
+            textLabel.setText(icon())
+            countdownTimer.stop()
+            timerStop()
         }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + addedTime! + 7) {
-            self.pushController(withName: "doneView", context: nil)
-        }
+    }
+    
+    func icon() -> String {
+         if isRunning{
+             return startWash
+      
+         }else{
+              
+             return stopWash
+         }
+          
      }
     
-    func firstTimer(){
-        
-        let interval:TimeInterval = 4.0
-        
+    
+    func timerReset(){
+         // A method to reset timer to 0 and start timer
+         let interval:TimeInterval = 5.0
         timerGroup.setBackgroundImageNamed("initial")
         timerGroup.startAnimatingWithImages(in: NSRange(location:0, length: 120), duration: interval, repeatCount: 1)
-            
-        textLabel.setText(self.soapArray.randomElement())
-        
-        let time = Date(timeIntervalSinceNow: interval)
-        countdownTimer.setDate(time)
-        countdownTimer.start()
-        
-        if timer.isValid {
-            print("isvalid")
-            timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(timerDidEnd(timer:)), userInfo: nil, repeats: false)
-        }
-    }
+         //Control the timer control on the interface
+         countdownTimer.stop()
+         let time  = Date(timeIntervalSinceNow: interval)
+         countdownTimer.setDate(time)
+         countdownTimer.start()
+          
+         //control the runLoop timer
+         if intervalTimer.isValid{intervalTimer.invalidate()} //shut off timer if on
+        intervalTimer = Timer.scheduledTimer(timeInterval: interval ,
+             target: self,  //Object to target when done
+            selector: #selector(timerDidEnd(timer:)), //Method on the object
+             userInfo: nil, //Extra user info, most likely a dictionary
+             repeats: false) //Repeat of not
+          
+     }
     
-    func secondTimer() {
-        
-        timerStop()
-        
-        let interval = Double(labelText!)
-        
-        timerGroup.setBackgroundImageNamed("progresscircle")
-        timerGroup.startAnimatingWithImages(in: NSRange(location:0, length: 120), duration: interval, repeatCount: 1)
-        
-        self.textLabel.setText(self.sayingsArray.randomElement())
-        guard let timeInterval = labelText else {
-            return
-        }
-        
-        let myDate = Date(timeIntervalSinceNow: timeInterval + 1)
-        self.countdownTimer.setDate(myDate)
-        self.countdownTimer.start()
-    }
     
     func timerStop(){
-           timer.invalidate()
-       }
-    
-    @objc
-    func timerDidEnd(timer:Timer) {
-        isRunning = !isRunning
-        firstTimer()
+        intervalTimer.invalidate()
     }
+    
+    
+     
+    func startSecondTimer() {
+        
+                let timerValue = intervalValue
+        // A method to reset timer to 0 and start timer
+               let interval:TimeInterval = timerValue
+               //Control the timer control on the interface
+        
+            timerGroup.setBackgroundImageNamed("progresscircle")
+            timerGroup.startAnimatingWithImages(in: NSRange(location:0, length: 120), duration: interval, repeatCount: 1)
+
+               countdownTimer.stop()
+               let time  = Date(timeIntervalSinceNow: interval)
+               countdownTimer.setDate(time)
+               countdownTimer.start()
+                
+               //control the runLoop timer
+               if intervalTimer.isValid{intervalTimer.invalidate()} //shut off timer if on
+              intervalTimer = Timer.scheduledTimer(timeInterval: interval ,
+                   target: self,  //Object to target when done
+                selector: #selector(secondTimerDidEnd(timer:)), //Method on the object
+                   userInfo: nil, //Extra user info, most likely a dictionary
+                   repeats: false) //Repeat of not
+    }
+    
+    
+   
+    @objc func timerDidEnd(timer:Timer){
+        //When we reach end of an workout interval, switch workout type
+            isRunning = !isRunning
+        
+         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.startSecondTimer()
+                }
+        
+            
+        //}
+    }
+    
+    @objc func secondTimerDidEnd(timer:Timer){
+        //When we reach end of an workout interval, switch workout type
+            isRunning = !isRunning
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.pushController(withName: "doneView", context: nil)
+            }
+        
+            
+        //}
+    }
+    
+    
+    
+        override func awake(withContext context: Any?) {
+            super.awake(withContext: context)
+    
+            intervalValue = context as! Double
+            
+            textLabel.setText(icon())
+            startRunning()
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    let currentDate = Date()
+//    let soapArray = ["Get some soap!","Prep phase","Lather up!", "Soapy, soap","Wash in 3!"]
+//    let sayingsArray = ["Wash those hands!", "Clean up!","","Water time!","Rinse, Repeat!"]
+//
+//    var isRunning = true
+//    var timer = Timer()
+//    var labelText:TimeInterval?
+//
+//    override func awake(withContext context: Any?) {
+//        super.awake(withContext: context)
+//
+//
+//        labelText = context as? TimeInterval
+//        let addedTime = context as? Double
+//
+//        firstTimer()
+//
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+//
+//            self.secondTimer()
+//        }
+//
+//        DispatchQueue.main.asyncAfter(deadline: .now() + addedTime! + 7) {
+//            self.pushController(withName: "doneView", context: nil)
+//        }
+//     }
+//
+//    func firstTimer(){
+//
+//        let interval:TimeInterval = 4.0
+//
+//        timerGroup.setBackgroundImageNamed("initial")
+//        timerGroup.startAnimatingWithImages(in: NSRange(location:0, length: 120), duration: interval, repeatCount: 1)
+//
+//        textLabel.setText(self.soapArray.randomElement())
+//
+//        let time = Date(timeIntervalSinceNow: interval)
+//        countdownTimer.setDate(time)
+//        countdownTimer.start()
+//
+//        if timer.isValid {
+//            print("isvalid")
+//            timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(timerDidEnd(timer:)), userInfo: nil, repeats: false)
+//        }
+//    }
+//
+//    func secondTimer() {
+//
+//        timerStop()
+//
+//        let interval = Double(labelText!)
+//
+//        timerGroup.setBackgroundImageNamed("progresscircle")
+//        timerGroup.startAnimatingWithImages(in: NSRange(location:0, length: 120), duration: interval, repeatCount: 1)
+//
+//        self.textLabel.setText(self.sayingsArray.randomElement())
+//        guard let timeInterval = labelText else {
+//            return
+//        }
+//
+//        let myDate = Date(timeIntervalSinceNow: timeInterval + 1)
+//        self.countdownTimer.setDate(myDate)
+//        self.countdownTimer.start()
+//    }
+//
+//    func timerStop(){
+//           timer.invalidate()
+//       }
+//
+//    @objc
+//    func timerDidEnd(timer:Timer) {
+//        isRunning = !isRunning
+//        firstTimer()
+//    }
 }
